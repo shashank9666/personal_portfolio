b5a67163344e9eb4ad1fab4becfc9d1ced42a58c
Shashank Shetty <shettyshashank2002@gmail.com>
2025-07-02 11:11:32 +0530

    Bike fixed

--- src/components/scene/Bike.jsx
+++ src/components/scene/Bike.jsx
@@ -1,7 +1,7 @@
 import { useGLTF } from "@react-three/drei";
 
 const Bike = (props) => {
-  const scene = useGLTF("/models/2022_yamaha_r1.glb");
+  const { scene } = useGLTF("/models/2022_yamaha_r1.glb");
   return <primitive object={scene} {...props} />;
 };
 
